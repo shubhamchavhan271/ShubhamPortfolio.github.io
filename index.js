b5b3a76939951427d768bd1c84d0edcc694ce76d
shubhamchavhan271 <shubhamchavhan271@gmail.com>
2023-03-09 11:18:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let nav = document.querySelector('.navbar');
+
+window.onscroll = function () {
+  if(document.documentElement.scrollTop > 20) {
+    nav.classList.add('header-scrolled');
+  }else {
+    nav.classList.remove('header-scrolled');
+  }
+}
+
+
+//  nav hide 
+
+   let navBar = document.querySelectorAll(".nav-link");
+   let navCollapse = document.querySelector(".nav-collapse.collapse");
+
+   navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+      navCollapse.classList.remove("show");
+    })
+   })
